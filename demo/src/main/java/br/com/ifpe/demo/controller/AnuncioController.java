ed8d5773d4c956c0f984d0559b9a503cc3b0f390
nielsonj13 <nielsoncorinthiano@gmail.com>
2025-05-09 15:28:31 -0300

    implementação do editar anuncio

--- demo/src/main/java/br/com/ifpe/demo/controller/AnuncioController.java
+++ demo/src/main/java/br/com/ifpe/demo/controller/AnuncioController.java
@@ -1,15 +1,22 @@
 package br.com.ifpe.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import br.com.ifpe.demo.model.Anuncio;
 import br.com.ifpe.demo.model.Usuario;
 import br.com.ifpe.demo.repository.AnuncioRepository;
 import br.com.ifpe.demo.repository.UsuarioRepository;
 import br.com.ifpe.demo.service.AnuncioService;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/anuncios")
@@ -51,6 +58,25 @@ public class AnuncioController {
     public void deletarAnuncio(@PathVariable Long id) {
         anuncioRepository.deleteById(id);
     }
+
+    @PutMapping("/usuario/{usuarioId}/{anuncioId}")
+    public Anuncio editarAnuncio(@PathVariable Long usuarioId, @PathVariable Long anuncioId, @RequestBody Anuncio anuncio) {
+        Usuario usuario = usuarioRepository.findById(usuarioId).orElseThrow();
+        Anuncio anuncioExistente = anuncioRepository.findById(anuncioId).orElseThrow();
+        
+        // Atualiza os dados do anúncio existente
+        anuncioExistente.setMarca(anuncio.getMarca());
+        anuncioExistente.setModelo(anuncio.getModelo());
+        anuncioExistente.setPreco(anuncio.getPreco());
+        //anuncioExistente.setAnoFabricacao(anuncio.getAnoFabricacao());
+        //anuncioExistente.setAnoModelo(anuncio.getAnoModelo());
+        anuncioExistente.setDescricao(anuncio.getDescricao());
+        //anuncioExistente.setImagens(anuncio.getImagens());
+        
+        // Salva o anúncio atualizado
+        return anuncioRepository.save(anuncioExistente);
+    }
+
 
     // @GetMapping
     // public List<Anuncio> listarAnuncio(@RequestParam(required = false) String termo) {
