895db82dca4d5a7a5a01b4967c39fd8573be836d
nielsonj13 <nielsoncorinthiano@gmail.com>
2025-05-22 17:16:12 -0300

    Implementação do spring security

--- demo/src/main/java/br/com/ifpe/demo/config/SecurityConfig.java
+++ demo/src/main/java/br/com/ifpe/demo/config/SecurityConfig.java
@@ -6,25 +6,48 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.http.HttpMethod;
+import java.util.List;
+
 
 @Configuration
 public class SecurityConfig {
-	
-	@Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+     http
+        .cors()
+        .and()
+        .csrf().disable()
+        .authorizeHttpRequests(auth -> auth
+			.requestMatchers("/", "/login", "/usuarios/cadastrar", "/anuncios","/public/**").permitAll()
+            .requestMatchers(HttpMethod.OPTIONS,  "/**").permitAll() // libera preflight
+            .anyRequest().authenticated()
+        )
+        .httpBasic();
+    return http.build();
+}
+  
+  @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:5173"));  // URL do seu frontend
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
-	
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http
-			.authorizeHttpRequests(auth -> auth
-				.anyRequest().permitAll()  // Permite todas as requisições
-			)
-			.csrf(csrf -> csrf.disable())  // Desativa CSRF
-			.httpBasic(httpBasic -> {});  // Permite autenticação básica
-	
-		return http.build();
-	}
-	 
-	 }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
+
