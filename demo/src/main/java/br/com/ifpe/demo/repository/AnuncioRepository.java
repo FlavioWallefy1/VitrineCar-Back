edaf281440bdb893482ab53ad3c51049afedadf3
FlavioWallefy1 <fwso@discente.ifpe.edu.br>
2025-05-24 18:31:16 -0300

    Adicionando método para contar anúncios recentes no repositório Anuncio

--- demo/src/main/java/br/com/ifpe/demo/repository/AnuncioRepository.java
+++ demo/src/main/java/br/com/ifpe/demo/repository/AnuncioRepository.java
@@ -3,9 +3,13 @@ package br.com.ifpe.demo.repository;
 import br.com.ifpe.demo.model.Anuncio;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface AnuncioRepository extends JpaRepository<Anuncio, Long> {
     List<Anuncio> findByUsuarioId(Long usuarioId);
+
     List<Anuncio> findByTituloContainingIgnoreCaseOrDescricaoContainingIgnoreCase(String titulo, String descricao);
+
+    long countByUsuarioIdAndDataCriacaoAfter(Long usuarioId, LocalDateTime dataCriacao);
 }
