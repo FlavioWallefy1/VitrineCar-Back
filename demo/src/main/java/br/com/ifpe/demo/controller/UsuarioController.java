0930cf1706a682b2eddaa0d8378fa8e6f2b216ac
nielsonj13 <nielsoncorinthiano@gmail.com>
2025-06-13 17:07:05 -0300

    Implementação de metodo para atualização da navbar

--- demo/src/main/java/br/com/ifpe/demo/controller/UsuarioController.java
+++ demo/src/main/java/br/com/ifpe/demo/controller/UsuarioController.java
@@ -1,9 +1,11 @@
 package br.com.ifpe.demo.controller;
 
 import br.com.ifpe.demo.model.Usuario;
+import br.com.ifpe.demo.repository.UsuarioRepository;
 import br.com.ifpe.demo.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.Authentication;
 
 import java.util.List;
 
@@ -12,12 +14,21 @@ import java.util.List;
 public class UsuarioController {
 
     @Autowired
+    private UsuarioRepository usuarioRepository;
+    @Autowired
     private UsuarioService usuarioService;
 
     @GetMapping
     public List<Usuario> listarUsuarios() {
         return usuarioService.listarUsuarios();
     }
+
+    @GetMapping("/logado")
+    public Usuario getUsuarioAutenticado(Authentication authentication) {
+        String email = authentication.getName(); // pega o email do token
+        return usuarioRepository.findByEmail(email).orElse(null);
+    }
+
 
     @PostMapping
     public Usuario criarUsuario(@RequestBody Usuario usuario) {
